0ae70b081eb6c2072f80cd9dc3dc0f9985742ed7
RovanIce <michael.green.701@my.csun.edu>
2025-09-26 12:23:45 -0700

    test 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Weather } from './weather/weather';
+import { Author } from './author/author';
+import { Genre } from './genre/genre';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home,pathMatch: 'full'},
+    {path: 'weather', component: Weather},
+    {path: 'city', component: Author},
+    {path: 'country', component: Genre}
+];
--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,11 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { WeatherData } from '.../weater-data';
 
 @Component({
   selector: 'app-weather',
   imports: [],
   templateUrl: './weather.html',
-  styleUrl: './weather.css'
+  styleUrl: './weather.scss'
 })
 export class Weather {
-
+  forecasts: WeatherData[] = [];
+  
+  constructor(http: HttpClient) {
+    http.get<WeatherData[]>('http://localhost:5032/weatherforecast').subscribe(result => {
+      this.forecasts= result;
+    });
+  }
 }
